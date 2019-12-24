d655b443a59b10bd3def7ab78549598835721373
Anish Karki <karkianish@hotmail.com>
2019-12-24 00:59:03 -0700

    Create Profile.ps1

---
+++ powershell/Profile.ps1
@@ -0,0 +1,16 @@
+echo "Loading profile..."
+
+Set-ExecutionPolicy RemoteSigned -Scope Process
+Set-PSReadLineOption -EditMode Vi -BellStyle None -ViModeIndicator Cursor
+# add timestamp to prompt. 
+$GitPromptSettings.DefaultPromptPrefix = "$(get-date -Format HH:mm:ss.fff) "
+# set prompt color
+$GitPromptSettings.DefaultForegroundColor = 'yellow'
+
+import-module posh-git
+import-module zlocation
+
+$conemuSettings = "$env:userprofile\scoop\apps\cmder\current\vendor\conemu-maximus5\ConEmu.xml";
+$vimrc = "$env:userprofile\_vimrc";
+
+cd $env:userprofile
